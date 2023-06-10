11c3d4fe2b0a4859eca99868d7a246d856a48a61
MalaikaArif26 <128815489+MalaikaArif26@users.noreply.github.com>
2023-06-10 11:07:37 -0700

    Add files via upload

---
+++ javascript3/assignment 17-20/app.js
@@ -0,0 +1,133 @@
+//------------------QUESTION NO 1 -----------------
+
+// let arr = [[1,2,3], [4,5,6], [7,8,9]]
+// // document.write(arr)
+// for (var i = 0; i < 3; i++){
+//     for(var b = 0; b < 3; b++){
+//         document.write(arr[i][b]+ " " );
+//     }
+//     document.write("<br>")
+// }
+
+
+
+//------------QUESTION NO 2---------------------------
+
+let arr =[[0,1,2,3], [1,0,2,0], [2,1,0,1]];
+for (var i = 0; i < 4; i++){
+    for(var b = 0; b < 4; b++){
+        document.write(arr[i][b]+ " " );
+    }
+    document.write("<br>")
+}
+
+
+
+
+
+//-----------------QUESTION NO 3--------------------------
+
+// for (i=1; i<=10; i++){
+//     document.write(i + "<br>");
+// }
+
+
+//----------------------QUESTION NO 4----------------------
+
+// let table = +prompt("Enter a number")
+// document.write("<b>OUTPUT</b> <br>")
+// for (i=1; i<=10; i++){
+//     document.write("<br>")
+//     document.write(table + " X " +i+ " = " +table*i)
+// }
+
+//--------------QUESTION NO 5----------------------\
+
+// let fruit = ["Apple", "Banana", "Mango", "Orange","Strawberry"];
+// document.write(fruit + "<br>");
+// document.write("<br>");
+
+// for (i=0;i<fruit.length;i++){
+//     document.write("Element at " + [i] + "  index is " +  fruit[i] + "<br>");
+// }
+
+//----------------QUESTION NO 6----------------------/
+
+// //(a) counting 
+// document.write("<h1>COUNTING</h1>");
+// for (i=1;i<=15;i++){
+//     document.write(i +  ",");
+// }
+
+// //(b) reverse counting
+// document.write("<h1>REVERSE COUNTING</h1>");
+// for (i=15 ;i >= 1 ; i--){
+//     document.write(i +  ",");
+// }
+
+// //(c) Even
+// document.write("<h1>EVEN</h1>");
+// for ( i=1 ; i<=20; i++ ){
+//    if(i % 2 !=1){
+//     document.write(i +  ",");
+//    }
+// }
+
+// //(d) Odd
+// for ( i=1 ; i<=20; i++ ){
+//     if(i % 2 !=0){
+//      document.write(i +  ",");
+//     }
+//  }
+
+// //(e)
+// document.write("<h1>Series</h1>");
+//  for ( i=1 ; i<=20; i++ ){
+//     if(i % 2 !=1){
+   
+//      document.write(i  + "k" +  "," );
+//     }
+//  }
+
+//----------------------QUESTION NO 7-----------------
+// let user = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?");
+// let flag = "False"
+
+// let bakery = ["cake", "apple pie", "cookie", "chips", "patties"];
+// for (var i = 0; i <= bakery.length; i++){
+//     if (user === bakery[i]){
+//         flag = "true"
+//     }
+//     }
+//     if(flag === "true"){
+//         document.write(user + " is available at index " + bakery[i]  )
+//     }
+//     else{
+//         document.write("Not found")
+//     }
+
+
+
+
+
+
+//-------------QUESTION NO 8-------------------------
+
+
+// let A = [24,53,78,91,12];
+// let largest = Math.max(...A);
+// console.log(largest);
+
+//-------------------QUESTION NO 9--------------------
+
+// let A = [24,53,78,91,12];
+// let largest = Math.min(...A);
+// console.log(largest);
+
+//------------------QUESTION NO 10---------------------
+
+// for (i=1; i<=20; i++){
+//     document.write(i*5 + "<br>")
+// }
+
+
