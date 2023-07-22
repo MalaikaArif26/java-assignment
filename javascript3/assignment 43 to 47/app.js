f336928c7624998b940f59a531c81f7552cb0e8e
MalaikaArif26 <128815489+MalaikaArif26@users.noreply.github.com>
2023-07-22 04:23:43 -0700

    Add files via upload

---
+++ javascript3/assignment 43 to 47/app.js
@@ -0,0 +1,48 @@
+//----------------------QUESTION NO 1--------------------------------
+
+// function abd(){
+//     alert("Thank you for purchasing")
+// }
+// function abc(){
+//     alert("Thank you for press link")
+// }
+ //------------------QUESTION NO0 2-=----------------------------
+
+
+// function deleteRow(button) {
+//     var row = button.parentNode.parentNode;
+//     row.parentNode.removeChild(row);
+//   }
+
+
+//---------------------QUESTION NO 3-------------------------------
+// var a = "./images/hut image.jpg";
+// var b = "./images/beauitful.jpeg";
+
+// function imgchange(){
+//     var imageElement = document.getElementById('image');
+//     imageElement.src = b;
+// }
+// function img(){
+//     var imageElement = document.getElementById('image').style.width="100%";
+//     imageElement.src = a;
+// }
+//------------------------QUESTIUON NO 4------------------------------
+
+
+// var counter = 0;
+// function update(){
+//     var counterElement = document.getElementById('counter');
+//     counterElement.innerText=counter;
+// }
+
+//------------------------QUESTION NO 5---------------------
+
+// function inc(){
+//     counter++;
+//     update();
+// }
+// function dec(){
+//     counter--;
+//     update();
+// }
